70d2e6883cf1e636898d241f53ac3da480b96025
Charles Huurman <ch@utopian.ink>
2023-12-06 14:18:22 +0100

    Update database configuration for production

--- config/config.js
+++ config/config.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
-const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    // On Heroku, JawsDB
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    // Locally, use your local database configuration
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: process.env.DB_HOST,
-        dialect: 'mysql' // or 'postgres', 'sqlite', etc.
-    });
-}
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+  },
+};
